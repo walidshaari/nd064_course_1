6ecd4ae9d1ec8ea391524c24dc8a5d6e78489598
Walid Shaari <walid.shaari@gmail.com>
2021-09-11 11:53:48 +0300

    create a Dockerfile task

--- exercises/README.md
+++ exercises/README.md
@@ -1,1 +1,10 @@
 
+
+uninstall k3s:
+   /usr/local/bin/k3s-uninstall.sh
+
+Errates:
+- fix k3s containercreateError by installing apparmor
+  https://documentation.suse.com/sles/12-SP4/html/SLES-all/cha-apparmor-start.html
+  https://github.com/rancher/k3os/issues/702
+  zypper in -t pattern apparmor
--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,13 +1,22 @@
 import sqlite3
+import os, sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+
+
+## Global variabls
+db_connection_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global   db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    #increment number of connections metrics
+    db_connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -16,6 +25,7 @@ def get_post(post_id):
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
+    ## app.logger.info('Article %s retrieved!' %post)
     return post
 
 # Define the Flask application
@@ -36,13 +46,43 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('A non-existing article is accessed and a 404 page is returned')
       return render_template('404.html'), 404
     else:
+      app.logger.info("Article %s retrieved," %post["title"])
       return render_template('post.html', post=post)
 
+# Define healthz endpoint
+@app.route('/healthz')
+def healthz():
+  response = app.response_class(
+            response=json.dumps({"result":" OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+  return response
+
+def post_count():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute('select count(*) from posts')
+    return cursor.fetchone()[0]
+
+# Define metrics endpoint
+@app.route('/metrics')
+def metrics():
+  response = app.response_class(
+            response=json.dumps({"db_connection_count": db_connection_count, "post_count": post_count() }),
+            status=200,
+            mimetype='application/json'
+    )
+
+  return response
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About Us page retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +100,26 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info('New article created %s ' %title)
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+  loglevel = os.getenv("LOGLEVEL", "DEBUG").upper()
+  loglevel = (
+      getattr(logging, loglevel)
+      if loglevel in ["CRITICAL", "DEBUG", "ERROR", "INFO", "WARNING",]
+      else logging.DEBUG
+  )
+  # Set logger to handle STDOUT and STDERR
+
+  stdout_handler = logging.StreamHandler(sys.stdout)
+  stderr_handler = logging.StreamHandler(sys.stderr)
+  handlers = [stderr_handler, stdout_handler]
+  format_output = '%(asctime)s - %(name)s - %(message)s'
+
+  logging.basicConfig(format=format_output, level=loglevel, handlers=handlers)
+
+  app.run(host='0.0.0.0', port='3111')
